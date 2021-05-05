5880026c98d6228db112c8991c24e0ac6a6b683a
romdim <rom-dim@hotmail.com>
2021-05-05 13:58:08 +0200

    Added aspect ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: ["./src/**/*.svelte"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    aspectRatio: {
+      9: '9',
+      16: '16',
+    },
     extend: {
+      colors: {
+        'efi': '#F53994',
+        'tat': '#FDF6E3',
+        'tat2': '#859900'
+      },
       fontFamily: {
         sans: ['Quicksand', ...defaultTheme.fontFamily.sans],
+        montserrat: ['Montserrat']
       },
       screens: {
         //   sm: '640px',
@@ -14,10 +24,15 @@ module.exports = {
         //   lg: '1024px',
         //   xl: '1280px',
         //'2xl': '1536px',
+        '3xl': '1921px'
       },
       textColor: {
-        'hover': '#f53994'
+        'hover': '#F53994',
+        'tat': '#586E75',
       }
     },
   },
-}
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
+};
