31db100c3ae8f0346d946fb761cce2213898e6bc
romdim <rom-dim@hotmail.com>
2021-05-07 03:20:41 +0200

    Added all svgs + images (unoptimised yet)

---
+++ src/images/eat/LogoBig.svelte
@@ -0,0 +1,7 @@
+<svg viewBox="0 0 508 285.75" xmlns="http://www.w3.org/2000/svg" aria-labelledby="logoTitle logoDesc" role="img" class="{$$props.class}">
+  <title id="logoTitle">eat Logo</title>
+  <desc id="logoDesc">Big Logo (16:9) for Project eat</desc>
+  <rect x=".2642" y=".2642" width="507.47" height="285.22" fill="#f4bac8"/>
+  <text transform="translate(166.51 51.03)" fill="#a40607" font-family="'Cantata One'" font-size="49.389" style="line-height:1.25"><tspan x="47.625" y="108.425">eat</tspan></text>
+  <path fill="none" stroke="#7288b9" stroke-width="1.852" d="M6.218 0v285.75M501.78 0v285.75M0 279.53h508M0 6.218h508M496.49 0v285.75M0 11.509h508M0 274.24h508M11.509 0v285.75"/>
+</svg>
---
+++ src/images/tat/Logo.svelte
@@ -0,0 +1,6 @@
+<svg viewBox="0 0 508 285.75" xmlns="http://www.w3.org/2000/svg" aria-labelledby="logoTitle logoDesc" role="img" class="{$$props.class}">
+  <title id="logoTitle">TAT Logo</title>
+  <desc id="logoDesc">Logo for Project TAT</desc>
+  <path d="M159.452 105.832v6.773h26.034v27.805h7.831v-27.805h26.035v-6.773h-59.9zm90.703 0L234.35 140.41h7.808l11.808-26.428 11.809 26.428h7.914l-15.807-34.578h-7.726zm38.486 0v6.773h26.034v27.805h7.832v-27.805h26.034v-6.773h-59.9zM185.486 153.47v26.445h7.832V153.47h-7.832zm42.894 0l-12.089 26.445h8.149l8.89-19.79 2.858-6.35.136-.305h-7.943zm43.23 0l.136.305 2.858 6.35 8.89 19.79h8.255l-12.09-26.445h-8.049zm43.066 0v26.445h7.832V153.47h-7.832z" fill="#586e75"/>
+  <path d="M142.88 146.91h222.25" stroke="#859900" stroke-linecap="round" stroke-width="4.763"/>
+</svg>
