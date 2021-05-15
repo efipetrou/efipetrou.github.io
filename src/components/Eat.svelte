317ac4f73504a3d0c92aac3e077b979d0085444d
romdim <rom-dim@hotmail.com>
2021-05-15 21:05:04 +0200

    Circle + mobile eat version

--- src/components/Eat.svelte
+++ src/components/Eat.svelte
@@ -24,6 +24,7 @@
   import Logo from '../images/eat/Logo.svelte';
   import WeeklyPlanner from '../images/eat/weekly-planner.jpg';
   import Bookshelf from '../images/eat/bookshelf.jpg';
+  import Circle from './eat/Circle.svelte';
   import Cutlery from '../images/eat/gravure/cutlery.png';
   import Calendar from '../images/eat/gravure/calendar.png';
   import GroceryList from '../images/eat/gravure/grocery-list.png';
@@ -37,32 +38,39 @@
 </script>
 
 <style>
-  .eat-color-1 {
-    background-color: #733080;
+  .w-1\/10 {
+    width: 10%;
   }
-  .eat-color-2 {
-    background-color: #F4BAC8;
-  }
-  .eat-color-3 {
-    background-color: #A40607;
-  }
-  .eat-color-4 {
-    background-color: #728889;
-  }
-  .eat-color-5 {
-    background-color: #F0C595;
-  }
-  .eat-color-6 {
-    background-color: #F5EBC6;
+  .w-7\/8 {
+    width: 87.5%;
   }
 </style>
 
-<div class="sm:flex mb-4">
-  <Logo class="w-24 mr-4" />
-  <p class="text-sm sm:text-2xl sm:w-1/2">Eat web app has food as its protagonist & 3 primary functions: to gather recipes, organise a weekly planner & write down a grocery list, using old-fashioned recipe books as an aesthetic guide.</p>
+<div class="w-full sm:w-1/2 mb-4">
+  <Logo class="w-24 mr-4 float-left" />
+  <p class="text-sm sm:text-2xl">Eat web app has food as its protagonist & 3 primary functions: to gather recipes, organise a weekly planner & write down a grocery list, using old-fashioned recipe books as an aesthetic guide.</p>
 </div>
 
-<p class="text-right mt-8 text-sm sm:text-2xl">Stack of Weekly Planners</p>
+<div class="relative sm:flex w-full pl-4">
+  <h3 class="text-base">UX Research</h3>
+
+  <div class="text-sm sm:text-xl">
+    <p>After conducting a small scale UX Research with a target group of 5 individuals, aged 25-35, which is usually when people move out of their parental home & create their own, I figured out that one of the main pain points for everyone was: "What am I going to cook for tomorrow?"</p>
+    <p>The result? A tailor-made solution for everyone, a weekly planner through their own digital recipes with an auto-generate shuffling functionality.</p>
+  </div>
+
+  <figure class="w-2/3 m-auto">
+    <img src={WeeklyPlanner} alt="Eat Weekly Planner">
+    <figcaption class="text-center mt-2 text-sm sm:text-2xl">Stack of Weekly Planners</figcaption>
+  </figure>
+
+  <div class="border-l border-black h-32 w-px absolute left-0 top-1/2 transform -translate-y-1/2">
+    <div class="rounded-full h-4 w-4 sm:h-16 sm:w-16 absolute transform -left-4 top-1/2 translate-x-1/2 -translate-y-1/2 bg-efi">
+    </div>
+  </div>
+</div>
+
+<!-- <p class="text-right mt-8 text-sm sm:text-2xl">Stack of Weekly Planners</p>
 <div class="flex items-center my-4 w-full">
   <div class="w-1/2 text-sm sm:text-xl flex-shrink-0">
     <p>After conducting a small scale UX Research with a target group of 5 individuals, aged 25-35, which is usually when people move out of their parental home & create their own, I figured out that one of the main pain points for everyone was: "What am I going to cook for tomorrow?"</p>
@@ -72,9 +80,57 @@
   <div class="w-1/3 m-auto">
     <img src={WeeklyPlanner} alt="Eat Weekly Planner">
   </div>
+</div> -->
+
+<div class="relative sm:flex w-full pr-4">
+  <h3 class="text-base">Moodboard</h3>
+
+  <div class="text-sm sm:text-xl">
+    <p>The initial moodboard creates a unique atmosphere & communicates the experience of the forthcoming result.</p>
+    <p>Seeking inspiration from various sources enabled me to avoid sterile commonality. Cinema, vintage images & my old fashioned recipe book was the perfect combination to get ideas from.</p>
+    <p>The vibrant color palette which is influenced by Wes Anderson's masterpiece "The Grand Budapest Hotel”, cultivates intense emotions, whilst nostalgia is the primary feeling that arises from the gravure icons.</p>
+  </div>
+
+  <div class="flex items-center w-full">
+    <div class="flex-1 flex-col items-stretch space-y-4 h-full m-auto">
+      <Circle color="#733080" />
+      <Circle color="#F4BAC8" />
+      <Circle color="#A40607" />
+      <Circle color="#728889" />
+      <Circle color="#F0C595" />
+      <Circle color="#F5EBC6" />
+    </div>
+
+    <div class="w-7/8">
+      <div class="flex items-center">
+        <div class="w-3/5 px-4">
+          <div class="flex items-center">
+              <img src={Cutlery} alt="Eat Moodboard - Cutlery Gravure" class="w-2/3 object-contain">
+              <img src={GroceryList} alt="Eat Moodboard - Grocery List Gravure" class="w-1/3 object-contain">
+          </div>
+          <div class="flex items-center">
+              <img src={Calendar} alt="Eat Moodboard - Calendar Gravure" class="w-1/2 object-contain">
+              <img src={Tips} alt="Eat Moodboard - Tips Gravure" class="w-1/2 object-contain">
+          </div>
+        </div>
+        <img src={Bookshelf} alt="Eat Moodboard - Bookshelf" class="w-2/5 object-contain" />
+      </div>
+
+      <img src={RecipeBookFront} alt="Eat Moodboard - Recipe Book Front" class="w-full object-contain"/>
+    </div>
+
+  </div>
+  <div class="flex flex-row-reverse w-full">
+    <!-- <img src={RecipeBookCover} alt="Eat Moodboard - Recipe Book Cover" class="w-2/6 object-contain absolute left-1/3 transform -translate-x-1/2" /> -->
+  </div>
+
+  <div class="border-r border-black h-32 w-px absolute right-0 top-1/2 transform -translate-y-1/2">
+    <div class="rounded-full h-4 w-4 sm:h-16 sm:w-16 absolute transform -left-4 top-1/2 translate-x-1/2 -translate-y-1/2 bg-efi">
+    </div>
+  </div>
 </div>
 
-<p class="my-4 text-right text-sm sm:text-2xl">Moodboard</p>
+<!-- <p class="my-4 text-right text-sm sm:text-2xl">Moodboard</p>
 <div class="sm:flex items-center my-4 w-full">
   <div class="w-full sm:w-1/2 text-sm sm:text-xl">
     <p>The initial moodboard creates a unique atmosphere & communicates the experience of the forthcoming result.</p>
@@ -108,29 +164,71 @@
       </div>
     </div>
   </div>
+</div> -->
+
+<div class="relative sm:flex w-full pl-4">
+  <h3 class="text-base">Wireframes</h3>
+
+  <div class="text-sm sm:text-xl">
+    <p>In a social media addicted society I wanted to create a digital solution for recipes which would be free of the endless information bombardment that we experience in our everyday life.</p>
+    <p>Thus, my recipes would be shareable but not explorable and therefore no likes or comments would take place either.</p>
+  </div>
+
+  <figure class="w-2/3 m-auto">
+    <img src={ActionsMobile} alt="Actions">
+    <!-- <img src={ActionsDesktop} alt="Actions" class="w-full m-auto"> -->
+    <figcaption class="text-center mt-2 text-sm sm:text-2xl">Actions</figcaption>
+  </figure>
+
+  <div class="border-l border-black h-32 w-px absolute left-0 top-1/2 transform -translate-y-1/2">
+    <div class="rounded-full h-4 w-4 sm:h-16 sm:w-16 absolute transform -left-4 top-1/2 translate-x-1/2 -translate-y-1/2 bg-efi">
+    </div>
+  </div>
 </div>
 
+<div class="relative sm:flex w-full pr-4">
+  <h3 class="text-base">Next Steps</h3>
 
-<div class="my-4 text-right">
+  <div class="text-sm sm:text-xl">
+    <p>Next steps include the design of an MVP, launching the web app and performing a new UX research with live interviews of testing the product under observation.</p>
+  </div>
+
+  <div class="flex justify-items-start items-center">
+    <figure class="w-2/3 m-auto">
+      <img src={Desktop} alt="Recipe Wireframe Desktop">
+      <figcaption class="text-center mt-2 text-sm sm:text-2xl">Actions</figcaption>
+    </figure>
+    <figure class="w-1/4 m-auto">
+      <img src={Mobile} alt="Recipe Wireframe Mobile">
+      <figcaption class="text-center mt-2 text-sm sm:text-2xl">Actions</figcaption>
+    </figure>
+  </div>
+
+  <div class="border-r border-black h-32 w-px absolute right-0 top-1/2 transform -translate-y-1/2">
+    <div class="rounded-full h-4 w-4 sm:h-16 sm:w-16 absolute transform -left-4 top-1/2 translate-x-1/2 -translate-y-1/2 bg-efi">
+    </div>
+  </div>
+</div>
+
+<!-- <div class="my-4 text-right">
   <p>Wireframes</p>
   <p>Desktop & Mobile</p>
 </div>
-<!-- <img src={ActionsDesktop} alt="cutlery" class="w-full m-auto"> -->
-<img src={ActionsMobile} alt="cutlery" class="w-1/2 float-right">
+<img src={ActionsDesktop} alt="Actions" class="w-full m-auto">
+<img src={ActionsMobile} alt="Actions" class="w-1/2 float-right">
 
-<!-- <hr class="border-t-4 border-efi"/> -->
+<hr class="border-t-4 border-efi"/>
 
 <div class="flex justify-items-start items-center">
-  <img src={Desktop} alt="cutlery" class="w-2/3">
-  <img src={Mobile} alt="cutlery" class="pt-8 w-1/4">
-  <!-- <img src={Mobile} alt="cutlery" class="pt-8 w-1/4 border-l-4 border-efi"> -->
+  <img src={Desktop} alt="Recipe Wireframe Desktop" class="w-2/3">
+  <img src={Mobile} alt="Recipe Wireframe Mobile" class="pt-8 w-1/4">
 </div>
 
-<!-- <hr class="border-t-4 border-efi"/> -->
+<hr class="border-t-4 border-efi"/>
 
 <p class="w-1/2 text-sm sm:text-xl">In a social media addicted society I wanted to create a digital solution for recipes which would be free of the endless information bombardment that we experience in our everyday life.</p>
 <p class="w-1/2 text-sm sm:text-xl">Thus, my recipes would be shareable but not explorable and therefore no likes or comments would take place either.</p>
-<p class="w-1/2 text-sm sm:text-xl">Next steps include the design of an MVP, launching the web app and performing a new UX research with live interviews of testing the product under observation.</p>
+<p class="w-1/2 text-sm sm:text-xl">Next steps include the design of an MVP, launching the web app and performing a new UX research with live interviews of testing the product under observation.</p> -->
 
 <div class="w-full cursor-pointer mt-8" on:click={openTat}>
   <TatLogo class="w-full m-auto bg-tat" />
---
+++ src/components/eat/Circle.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let color: string = "black";
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2 2" aria-labelledby="logoTitle logoDesc" role="img" class="{$$props.class}">
+  <title id="logoTitle">Colored Circle</title>
+  <desc id="logoDesc">Colored circles for Eat Project</desc>
+  <circle cx="1" cy="1" r="1" stroke="none" fill={color} />
+</svg>
